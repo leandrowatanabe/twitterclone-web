230f8e3bde4b0662aa511bb9791be4f151f8131d
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-06-22 10:19:31 -0300

    fix: Atualizando estrutura da pastas

--- src/Pages/SignUp/index.jsx
+++ src/Pages/SignUp/index.jsx
@@ -19,7 +19,7 @@ export default function SignUp({ signInUser }){
 
     const formik = useFormik({
         onSubmit: async values => {
-            const res = await axios.post(`${import.meta.env.VITE_API_HOST}/signup`, {
+            const res = await axios.post(`${import.meta.env.VITE_API_HOST_DEV}/signup`, {
                 name: values.name,
                 username: values.username,
                 email: values.email,
@@ -40,67 +40,74 @@ export default function SignUp({ signInUser }){
 
     return(
         <>
-        <div className="h-full flex flex-col justify-center p-12 space-y-6">
-                <h1 className="text-platinum text-3xl font-bold">Crie sua conta</h1>
-                <form className='space-y-6' onSubmit={formik.handleSubmit}>
-                    <div className='space-y-2'>
-                        <Input 
-                            type="text"
-                            name="name"
-                            placeholder="Nome"
-                            value={formik.values.name}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}  
-                        />
-                        {(formik.touched.name && formik.errors.name) && 
-                            (<div className='text-red-500 text-sm'>{formik.errors.name}</div>)}
-                    </div>
-                    <div className='space-y-2'>
-                        <Input 
-                            type="text"
-                            name="username"
-                            placeholder="Nome de usuário"
-                            value={formik.values.username}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}  
-                        />
-                        {(formik.touched.username && formik.errors.username) && 
-                            (<div className='text-red-500 text-sm'>{formik.errors.username}</div>)}
-                    </div>
-                    <div className='space-y-2'>
-                        <Input 
-                            type="text"
-                            name="email"
-                            placeholder="E-mail"
-                            value={formik.values.email}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}  
-                        />
-                        {(formik.touched.email && formik.errors.email) && 
-                            (<div className='text-red-500 text-sm'>{formik.errors.email}</div>)}
-                    </div>
-                    <div className='space-y-2'>
-                        <Input 
-                            type="password"
-                            name="password"
-                            placeholder="Senha" 
-                            value={formik.values.password}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}  
-                        />
-                         {(formik.touched.password && formik.errors.password) && 
-                            (<div className='text-red-500 text-sm'>{formik.errors.password}</div>)}
-                    </div>
-                    <button
-                        type='submit'
-                        className="bg-birdBlue py-4 rounded-full w-full my-6 font-bold text-md"
-                        disabled={!formik.isValid || formik.isSubmitting}
-                    >
-                        {formik.isSubmitting ? 'Enviando...' : 'Cadastrar'}
-                    </button>
-                </form>
-    
-            <span className="text-silver text-sm text-center"> Ja tem uma conta? <a href="/login" className="text-birdBlue"> Acesse </a></span>
+        <div className="h-full flex justify-center">
+            <div className="bg-birdBlue lg:flex-1">
+                
+            </div>
+            <div  className="flex-1 flex justify-center items-center p-12 space-y-6 ">
+                <div className='max-w-md flex-1'>
+                    <h1 className="text-platinum text-3xl font-bold">Crie sua conta</h1>
+                    <form className='space-y-6' onSubmit={formik.handleSubmit}>
+                        <div className='space-y-2'>
+                            <Input 
+                                type="text"
+                                name="name"
+                                placeholder="Nome"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}  
+                            />
+                            {(formik.touched.name && formik.errors.name) && 
+                                (<div className='text-red-500 text-sm'>{formik.errors.name}</div>)}
+                        </div>
+                        <div className='space-y-2'>
+                            <Input 
+                                type="text"
+                                name="username"
+                                placeholder="Nome de usuário"
+                                value={formik.values.username}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}  
+                            />
+                            {(formik.touched.username && formik.errors.username) && 
+                                (<div className='text-red-500 text-sm'>{formik.errors.username}</div>)}
+                        </div>
+                        <div className='space-y-2'>
+                            <Input 
+                                type="text"
+                                name="email"
+                                placeholder="E-mail"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}  
+                            />
+                            {(formik.touched.email && formik.errors.email) && 
+                                (<div className='text-red-500 text-sm'>{formik.errors.email}</div>)}
+                        </div>
+                        <div className='space-y-2'>
+                            <Input 
+                                type="password"
+                                name="password"
+                                placeholder="Senha" 
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}  
+                            />
+                            {(formik.touched.password && formik.errors.password) && 
+                                (<div className='text-red-500 text-sm'>{formik.errors.password}</div>)}
+                        </div>
+                        <button
+                            type='submit'
+                            className="bg-birdBlue py-4 rounded-full w-full my-6 font-bold text-md"
+                            disabled={!formik.isValid || formik.isSubmitting}
+                        >
+                            {formik.isSubmitting ? 'Enviando...' : 'Cadastrar'}
+                        </button>
+                    </form>
+        
+                <span className="text-silver text-sm text-center"> Ja tem uma conta? <a href="/login" className="text-birdBlue"> Acesse </a></span>
+                </div>
+            </div>
         </div>
         </>
     )
