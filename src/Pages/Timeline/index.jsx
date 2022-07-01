4ba427ec2d7aa2ef8f7f0ea2ecc6870afe6c33c7
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-07-01 20:15:36 -0300

    feat: Adicionando botao de refresh de tweets

--- src/Pages/Timeline/index.jsx
+++ src/Pages/Timeline/index.jsx
@@ -7,7 +7,9 @@ import TweetForm from '../../components/TweetForm'
 import Tweet from '../../components/Tweet'
 
 export default function Timeline( {loggedInUser} ){
+  const [renderTweets, setRenderTweets] = useState([])
   const [tweets, setTweets] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
   
   async function getTweets() {
     const res = await axios.get(`${import.meta.env.VITE_API_HOST}/tweets`, {
@@ -15,22 +17,51 @@ export default function Timeline( {loggedInUser} ){
         Authorization: `Bearer ${loggedInUser.accessToken}`
       }
     })
+    setRenderTweets(await res.data)
+  }
+
+  async function refreshTweets(){
+    const res = await axios.get(`${import.meta.env.VITE_API_HOST}/tweets`, {
+      headers:{
+        Authorization: `Bearer ${loggedInUser.accessToken}`
+      }
+    })
     setTweets(res.data)
+    if(tweets.length > renderTweets.length && !isOpen){
+      setIsOpen(true)
+    }
+  }
+
+  function test(){
+    return(
+      isOpen
+      ?<div className="flex p-4 border-b border-silver justify-center" onClick={()=>{
+        setRenderTweets(tweets)
+        setTweets(renderTweets)
+        setIsOpen(false)
+      }}>
+          {tweets.length - renderTweets.length > 0? tweets.length - renderTweets.length : setIsOpen(false)} tweets
+        </div>
+      :<></>
+    )
   }
 
   useEffect(()=>{
     getTweets()
   },[])
 
-
-
+  useEffect(()=>{
+    const period = setInterval(refreshTweets, 2000)
+    return () => clearInterval(period)
+  },[tweets])
 
   return(
     <>
-    
       <TweetForm loggedInUser={loggedInUser} onSuccess={getTweets}/>
 
-      {tweets.length && tweets.map(tweet=>{return(
+      {test()}
+
+      {renderTweets.length && renderTweets.map(tweet=>{return(
         <Tweet 
           key={tweet.id} 
           name={tweet.user.name} 
@@ -40,7 +71,8 @@ export default function Timeline( {loggedInUser} ){
           likes={tweet.likes}
         >
           {tweet.text}
-        </ Tweet >)
+        </ Tweet >
+        )
       })}
       
     </>
